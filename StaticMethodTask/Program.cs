cb14aee45c7c378de2fff759adeee558706c05ba
Qadir088 <qadircabbarli55@gmail.com>
2024-04-08 02:28:32 +0400

    task solved

---
+++ StaticMethodTask/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace StaticMethodTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[0];
+            arr = arr.Add(1);
+            arr = arr.Add(2);
+            arr = arr.Add(3);
+            arr = arr.Add(4);
+            int sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+            Console.WriteLine("Answer: " + sum);
+        }
+    }
+
+    public static class Extensions
+    {
+        public static int[] Add(this int[] array, int newItem)
+        {
+            int[] newArray = new int[array.Length + 1];
+            Array.Copy(array, newArray, array.Length);
+            newArray[array.Length] = newItem;
+            return newArray;
+        }
+    }
+}
+
